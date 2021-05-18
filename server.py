0f8283922dc92a5074a25ef9a79a58211efdc84c
CsarChvz <cesarchavezrodriguez9@gmail.com>
2021-05-18 13:27:23 -0500

    sdasfas

--- server.py
+++ server.py
@@ -30,5 +30,20 @@ def handle(client):
             nicknames.remove
             break 
 
+def receive():
+    while True:
+        client, address = server.accept()
+        print(f"Connected with {str(address)}")
 
 
+        client.send("NICK".encode("ascii"))
+        nickname = client.recv(1024).decode('ascii')
+        clients.append(client)
+
+        print(f"Nickaname of the cliente is  {nickname}")
+        broadcast(f"{nickname} join to the chat".encode('ascii'))
+
+        client.send('Connected to the server!'.encode("ascii"))
+
+        thread = threading.thread(target=handle, args=(client,))
+        thread.start()
